d18ca53b23613562d67247d9f1b0fe101e769bee
lenovo <anisaauliafry08@gmail.com>
2022-03-31 10:19:46 +0700

    ini program representasi

--- src/main/kotlin/representasi.kt
+++ src/main/kotlin/representasi.kt
@@ -1,12 +1,10 @@
 fun main(args: Array<String>) {
-    val oneMillion = 1_000_000
-    val creditCardNumber = 1234_5678_9012_3456L
-    val socialSecurityNumber = 999_99_9999L
-    val hexBytes = 0xFF_EC_DE_5E
-    val bytes = 0b11010010_01101001_10010100_10010010
-    println("oneMillion = " + oneMillion)
-    println("creditCardNumber = " + creditCardNumber)
-    println("socialSecurityNumber = " + socialSecurityNumber)
-    println("hexBytes = " + hexBytes)
-    println("bytes = " + bytes)
+    val a: Int = 10000
+    println("a apakah identik dengan a atau a === a : " + (a === a))
+    val boxedA: Int? = a
+    val anotherBoxedA: Int? = a
+    println("boxedA apakah identik dengan anotherBoxedA atau boxedA === anotherBoxedA : " + (boxedA ===
+            anotherBoxedA))
+    println("boxedA apakah memiliki nilai yang sama dengan anotherBoxedA atau boxedA == anotherBoxedA : "
+            + (boxedA == anotherBoxedA))
 }
